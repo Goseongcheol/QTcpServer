361a5ff8764fc850efc28721b1ab75d8aff3df16
GoSeongCheol <tjdcjf0781@gmail.com>
2025-10-28 14:35:21 +0900

    READYREAD- switch ~ case, protocol

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -45,7 +45,8 @@ void MainWindow::newConnection()
     // 여기에 클라이언트 정보 받아서 사용자 목록에 추가 + 접속중인 사용자에게 목록 전송 (브로드캐스트)
     //
 
-    qDebug() << "Client connected ";
+    //원래는 여기서 log처리해야하지만 client 의 port와 ip데이터만 변경해서 local로 돌리려고 readyRead에서 처리 예정
+    // qDebug() << "Client connected ";
 
     clients.insert(client);
 }
@@ -56,15 +57,118 @@ void MainWindow::readyRead()
     QTcpSocket *client = qobject_cast<QTcpSocket*>(sender());
     if (!client) return;
 
-    QByteArray data = client->readAll();
-    qDebug() << "Received: " << data;
+    QByteArray packet = client->readAll();
 
-    // 추가할 내용
-    // 받은데이터 로그에 남기기(ui 표시 + 해당 날짜 폴더에 로그 기록하기
-    // 채팅 메세지와 connect연결 0x01 과 0x12로 구분해서 처리하면 될듯? CMD 패킷만 뜯어서 switch case 사용?
-    //
 
+    quint8 STX  = static_cast<quint8>(packet[0]);
+    quint8 CMD  = static_cast<quint8>(packet[1]);
+    quint8 lenH = static_cast<quint8>(packet[2]);
+    quint8 lenL = static_cast<quint8>(packet[3]);
+    quint16 LEN = (static_cast<quint16>(lenH) << 8) | lenL;
+
+
+    //패킷 형식 검증
+    if (packet.size() < 1 + 1 + 2 + LEN + 1 + 1) {
+        qDebug() << "packet size error";
+        //nack 보내기
+        return;
+    }
+
+    QByteArray data = packet.mid(4, LEN);
+    quint8 checksum = static_cast<quint8>(packet[4 + LEN]);
+    quint8 ETX      = static_cast<quint8>(packet[5 + LEN]);
+
+    // 체크섬 계산 (클라이언트와 동일)
+    quint32 sum = CMD + lenH + lenL;
+    for (unsigned char c : data)
+        sum += c;
+    quint8 expect = static_cast<quint8>(sum % 256);
+
+    // qDebug() << "STX: "  << STX << "ETX: " << ETX << "cehcksum :" << checksum << "ETX: " << expect ;
+
+    //STX, ETX 검증
+    if(STX != 2 || ETX != 3){
+        qDebug() << "STX OR ETX error";
+        //
+        //nack보내기
+        //
+        return;
+    }
+
+    //받은 checksum과 계산한 checksum 확인하기
+    if(checksum != expect)
+    {
+        qDebug() << "checksum error";
+        //
+        //nack보내기
+        //
+        return;
+    }
+
+
+    //CMD 에 맞게 따로 처리
+    switch (CMD){
+    case 1 :
+         qDebug() << "DATA:" << QString::fromUtf8(data);
+        //user_connect
+        qDebug() << "connect";
+
+
+        break;
+    case 2 :
+        //user_list
+        qDebug() << "user_list";
+
+
+        break;
+    case 3 :
+        //user_join
+        qDebug() << "user_join";
+
+
+        break;
+    case 4 :
+        //user_leave
+        qDebug() << "user_leave";
+
+
+        break;
+    case 8 :
+        //ack
+        qDebug() << "ack";
+
+
+        break;
+    case 9 :
+        //Nack
+        qDebug() << "nack";
+
+
+        break;
+    case 18 :
+        //chat으로 메세지 송수신 + 브로드캐스트
+        qDebug() << "chat ";
+
+
+        break;
+    default :
+        qDebug() << "DATA:" << QString::fromUtf8(data);
+        qDebug() << "none";
+
+        break;
+    }
+
+    qDebug() << "readyRead end";
 }
+
+// 추가할 내용
+// 받은데이터 로그에 남기기(ui 표시 + 해당 날짜 폴더에 로그 기록하기
+// 채팅 메세지와 connect연결 0x01 과 0x12로 구분해서 처리하면 될듯? CMD 패킷만 뜯어서 switch case 사용?
+//
+
+
+
+
 
 //클라이언트 연결이 종료되면 clients 목록에서 제거
 void MainWindow::disconnected()
